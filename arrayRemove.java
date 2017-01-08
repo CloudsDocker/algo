6a3d1dba282fc775cabba0ec2e0a4babfa9b2f9d
todd zhang <phray.zhang@gmail.com>
2017-01-08 17:06:47 +0800

    init add

---
+++ arrayRemove.java
@@ -0,0 +1,11 @@
+
+public class arrayRemove{
+	public static void main(String[] args) {
+		System.out.println("test to remove elements > given value in place");
+	}
+
+	static removeLargerElement(int[] input,int threshold){
+		// try to find elements larger than the threshold, and remove those elements in place
+		
+	}
+}
