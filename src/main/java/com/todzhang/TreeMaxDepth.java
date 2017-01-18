06b5d1014d33974bfdb47ab3af13bc258ac98588
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-18 11:16:01 +0800

    Update TreeMaxDepth.java

--- src/main/java/com/todzhang/TreeMaxDepth.java
+++ src/main/java/com/todzhang/TreeMaxDepth.java
@@ -9,8 +9,9 @@ public class TreeMaxDepth {
 		// TODO Auto-generated method stub
 
 		TreeMaxDepth inst=new TreeMaxDepth();		
-		TreeNode root=inst.initTree();
+		TreeNode root=TreeBuilder.initTree();
 		System.out.println("--- max length is :"+inst.maxDepth(root));
+		System.out.println("--- min length is :"+inst.minDepth(root));
 	}
 	
 	
@@ -22,29 +23,14 @@ public class TreeMaxDepth {
 		return 1+Math.max(maxDepth(node.left), maxDepth(node.right));
 	}
 	
-	private TreeNode initTree(){
+	private int minDepth(TreeNode node){
+		if(node==null){
+			return 0;
+		}
 		
-		TreeNode root=new TreeNode(1);
-		TreeNode node2=new TreeNode(2);
-		TreeNode node3=new TreeNode(3);
-		TreeNode node4=new TreeNode(4);
-		TreeNode node5=new TreeNode(5);
-		TreeNode node6=new TreeNode(6);
-		TreeNode node7=new TreeNode(7);
-		TreeNode node8=new TreeNode(8);
-		
-		root.left=node2;
-		root.right=node3;
-		
-		node2.left=node4;
-		node2.right=node5;
-		
-		node3.left=node6;
-		node3.right=node7;
-		
-		node4.left=node8;
-		
-		return root;
+		return 1+Math.min(maxDepth(node.left), maxDepth(node.right));
 	}
+	
+	
 
 }
