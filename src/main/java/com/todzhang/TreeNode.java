46fe2c66df98e765a99dc70e95c4fa32b1d0745c
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-18 09:44:20 +0800

    Create TreeNode.java

---
+++ src/main/java/com/todzhang/TreeNode.java
@@ -0,0 +1,13 @@
+package me.todzhang;
+
+public class TreeNode {
+
+	public  int value=Integer.MIN_VALUE;
+	
+	public  TreeNode left=null;
+	public  TreeNode right=null;
+	public TreeNode(int value) {
+		super();
+		this.value = value;
+	}
+}
