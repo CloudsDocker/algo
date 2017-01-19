e87e585d8a5480721280a8c3f8a27287fdb8ad5e
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-19 09:12:44 +0800

    Update StaticFactorySingleton.java

--- src/main/java/com/todzhang/StaticFactorySingleton.java
+++ src/main/java/com/todzhang/StaticFactorySingleton.java
@@ -1,7 +1,29 @@
 package me.todzhang;
 
-public class StaticFactorySingleton {
+import java.io.Serializable;
 
+
+public class StaticFactorySingleton implements Serializable{
+
+	/**
+	 * Another problem with conventional Singletons are that once you implement serializable
+	 *  interface they are no longer remain Singleton because readObject() method 
+	 *  always return a new instance just like constructor in Java. 
+	 *  you can avoid that by using readResolve() method and discarding newly created
+	 *   instance by replacing with Singeton as shwon in below example :
+
+	    //readResolve to prevent another instance of Singleton
+	    private Object readResolve(){
+	        return INSTANCE;
+	    }
+	This can become even more complex if your Singleton Class maintain state, 
+	as you need to make them transient, but witn Enum Singleton, Serialization is guarnateed by JVM.
+	 * 
+	 *
+	 */
+	private Object readResolve(){
+		return INSTANCE;
+	}
 	// this is private, static and final instance
 	// as it's static, so this variable will be
 	// initialized during class loading, so it's
@@ -18,4 +40,6 @@ public class StaticFactorySingleton {
 	}
 	
 
+	
+	
 }
