68bf8937f286df7f6257879c63fe6be14eabb1f5
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-19 09:12:27 +0800

    Update DoubleCheckedSingleton.java

--- src/main/java/com/todzhang/DoubleCheckedSingleton.java
+++ src/main/java/com/todzhang/DoubleCheckedSingleton.java
@@ -1,7 +1,29 @@
 package me.todzhang;
 
-public class DoubleCheckedSingleton {
+import java.io.Serializable;
 
+public class DoubleCheckedSingleton implements Serializable{
+
+	/**
+	 * Another problem with conventional Singletons are that once you implement serializable
+	 *  interface they are no longer remain Singleton because readObject() method 
+	 *  always return a new instance just like constructor in Java. 
+	 *  you can avoid that by using readResolve() method and discarding newly created
+	 *   instance by replacing with Singeton as shwon in below example :
+
+	    //readResolve to prevent another instance of Singleton
+	    private Object readResolve(){
+	        return INSTANCE;
+	    }
+	This can become even more complex if your Singleton Class maintain state, 
+	as you need to make them transient, but witn Enum Singleton, Serialization is guarnateed by JVM.
+	 * 
+	 *
+	 */
+	private Object readResolve(){
+		return INSTANCE;
+	}
+	
 	private static volatile DoubleCheckedSingleton INSTANCE;
 	
 	private DoubleCheckedSingleton(){};
