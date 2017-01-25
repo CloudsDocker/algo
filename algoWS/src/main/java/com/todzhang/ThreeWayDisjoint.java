4878f12c61d7e7726194275a200c999b3962c9f6
todd zhang <phray.zhang@gmail.com>
2017-01-25 23:13:11 +0800

    daily commit

---
+++ algoWS/src/main/java/com/todzhang/ThreeWayDisjoint.java
@@ -0,0 +1,39 @@
+package com.todzhang;
+
+public class ThreeWayDisjoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static boolean disjoint1(int[]  groupA, int[] groupB, int[] groupC){
+		for ( int i : groupA) {
+			for (int j : groupB) {
+				for (int k : groupC) {
+					if(i==j && j==k){
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	private static boolean disjoint2(int[]  groupA, int[] groupB, int[] groupC){
+		for ( int i : groupA) {
+			for (int j : groupB) {
+				if(i==j){
+					// add this checking to reduce complexitiy
+					for (int k : groupC) {
+						if(j==k){
+							return false;
+						}
+					}
+				}
+				
+			}
+		}
+		return true;
+	}
+}
