85a818d33bc46e8efd7c59d113089cd71862f755
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-26 10:55:57 +0800

    Update PrefixAverage.java

--- eclipseWS/src/com/todzhang/PrefixAverage.java
+++ eclipseWS/src/com/todzhang/PrefixAverage.java
@@ -11,7 +11,8 @@ public class PrefixAverage {
 
 		double[] res=new double[]{1,2,3,4,5,7,10,20,30,40};
 		System.out.println("input is :"+Arrays.toString(res));
-		System.out.println("output is:"+Arrays.toString(prefixAverage1(res)));
+		System.out.println("naiive approacl O(n*n), output is:"+Arrays.toString(prefixAverage1(res)));
+		System.out.println("linear bette approacl O(n), output is:"+Arrays.toString(prefixAverage2(res)));
 
 	}
 	
@@ -25,6 +26,23 @@ public class PrefixAverage {
 			for (int j = 0; j <=i; j++) { // be awre it's <=, instead of "<"
 				total+=x[j];				
 			}
+			a[i]=total/(i+1);			
+		}
+		return a;
+	}
+	
+	// previous implementation is O(n2), 
+	// while below with enhancement, the result is linear O(n)
+	// For greater efficiency, we can maintain the current prefix sum dynamically, 
+	// effectively computing x0 + x1 + ··· + xj as total + xj, 
+	// where value total is equal to the sum x0 + x1 + ··· + xj − 1, 
+	// when computed by the previous pass of the loop over j.
+	private static double[] prefixAverage2(double[] x){
+		int n=x.length;
+		double[] a =new double[n];
+		double total=0;
+		for (int i = 0; i < n; i++) {
+			total += x[i];
 			a[i]=total/(i+1);			
 		}
 		return a;
