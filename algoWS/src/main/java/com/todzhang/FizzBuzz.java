c9d5f2fbcd6c5a0b576d26e688cda970ff151267
todd zhang <phray.zhang@gmail.com>
2017-02-18 22:28:27 +0800

    daily commit

---
+++ algoWS/src/main/java/com/todzhang/FizzBuzz.java
@@ -0,0 +1,15 @@
+package com.todzhang;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void fizzBuzz(int n){
+		for(int i=0;i<=n;i++){
+			
+		}
+	}
+}
