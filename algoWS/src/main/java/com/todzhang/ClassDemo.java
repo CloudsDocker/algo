1a59794a00ef43206ee57c18a1ee35fe05fb1086
todd zhang <phray.zhang@gmail.com>
2017-02-19 09:42:22 +0800

    daily commit

---
+++ algoWS/src/main/java/com/todzhang/ClassDemo.java
@@ -0,0 +1,14 @@
+package com.todzhang;
+import static java.lang.System.out;
+
+public class ClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println(" class literal:"+byte[].class);
+		out.println(" for primitive,there are class object:"+Integer.TYPE);
+		Arrays.
+		
+	}
+
+}
